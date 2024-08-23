ac175aef1f31b50248fc1ddab977ba8f00143c14
Jingwen Lin <jingwenlin@Jingwens-MacBook-Pro.local>
2024-08-22 20:50:21 -0700

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
